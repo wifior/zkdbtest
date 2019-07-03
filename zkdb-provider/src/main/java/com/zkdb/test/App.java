49186e3801a01be9e314fe9a3a7646b0efe1a81d
葛会军 <45944979@qq.com>
2019-07-03 19:03:15 +0800

    框架搭建

---
+++ zkdb-provider/src/main/java/com/zkdb/test/App.java
@@ -0,0 +1,13 @@
+package com.zkdb.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
