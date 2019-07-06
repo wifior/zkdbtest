d0cbf8b5929bd84b097fc5c230c5c52dbb8f3794
葛会军 <45944979@qq.com>
2019-07-06 18:10:52 +0800

    调试成功

--- zkdb-web/src/main/java/com/zkdb/test/controller/TestController.java
+++ zkdb-web/src/main/java/com/zkdb/test/controller/TestController.java
@@ -2,6 +2,7 @@ package com.zkdb.test.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.zkdb.test.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("demo")
 public class TestController {
 
-    @Reference(version = "1.0.0")
+    @Reference
     private TestService testService;
 
     @RequestMapping("index/{name}")
